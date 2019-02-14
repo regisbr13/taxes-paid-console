2bccf8a580babc65edb2bfdd3fa11890c30a8ff0
Regis Lima <regislima1391@gmail.com>
2019-02-14 16:45:32 -0300

    Definidos construtores da classe Payer e definida a classe IndividualPayer com seus construtores e metodos

---
+++ TaxesPaidConsole/Entities/IndiviualPayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxesPaidConsole.Entities
+{
+    class IndiviualPayer : Payer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public IndiviualPayer()
+        {
+        }
+
+        public IndiviualPayer(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double TaxesPaid()
+        {
+            if(AnualIncome < 20000.00)
+            {
+                if(HealthExpenditures > 0.00)
+                {
+                    return AnualIncome * 0.15 - HealthExpenditures / 2.0;
+                }
+                return 2000.00 * 0.15;
+            }
+            if (HealthExpenditures > 0.00)
+            {
+                return AnualIncome * 0.25 - HealthExpenditures / 2.0;
+            }
+            return 2000.00 * 0.25;
+        }
+    }
+}
