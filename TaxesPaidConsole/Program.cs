6f06c6089dd7bf6452132ea8e8a6954a2a3ecbba
Regis Lima <regislima1391@gmail.com>
2019-02-14 17:10:02 -0300

    Criada tela de interacao com o usuario

--- TaxesPaidConsole/Program.cs
+++ TaxesPaidConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using TaxesPaidConsole.Entities;
 
 namespace TaxesPaidConsole
 {
@@ -6,7 +9,35 @@ namespace TaxesPaidConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Payer> payers = new List<Payer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Tax payer #{0} data:", i);
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(ch == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpend = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    payers.Add(new IndiviualPayer(name, anualIncome, healthExpend));
+                }
+                else if(ch == 'c')
+                {
+                    Console.Write("Number of employees: ");
+                    int employees = int.Parse(Console.ReadLine());
+                    payers.Add(new CompanyPayer(name, anualIncome, employees));
+                }
+            }
+
+
         }
     }
 }
