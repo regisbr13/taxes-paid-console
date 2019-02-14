d502b0c5173ecc72f4e476d47d95adba3f85a7fc
Regis Lima <regislima1391@gmail.com>
2019-02-14 16:32:24 -0300

    Definida a classe abstrata Payer com seus atributos e o metodo abstrato para calculo de impostos

---
+++ TaxesPaidConsole/Entities/Payer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxesPaidConsole.Entities
+{
+    abstract class Payer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public abstract double TaxesPaid();
+    }
+}
